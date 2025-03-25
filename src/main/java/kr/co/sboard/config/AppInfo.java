4a39dd37445b0862f0b187248f392ba9f6f18444
sohyeon <sohyoun26@gmail.com>
2025-03-25 13:32:20 +0900

    commit 0.0.3

--- src/main/java/kr/co/sboard/config/AppInfo.java
+++ src/main/java/kr/co/sboard/config/AppInfo.java
@@ -1,9 +1,6 @@
 package kr.co.sboard.config;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.beans.factory.annotation.Value;
 
 @Data
@@ -12,9 +9,10 @@ import org.springframework.beans.factory.annotation.Value;
 @Builder
 public class AppInfo {
 
-    @Value("${spring.application.name}") // application.yml 파일이 속성값으로 초기화
+    @Value("${spring.application.name}") // application.yml 파일에 속성값으로 초기화
     private String appName;
 
-    @Value("${version}")
+    @Value("${spring.application.version}")
     private String appVersion;
+
 }
--- src/main/java/kr/co/sboard/config/WebMvcConfig.java
+++ src/main/java/kr/co/sboard/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package kr.co.sboard.config;
 
 import kr.co.sboard.interceptor.AppInfoInterceptor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
